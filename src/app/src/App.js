f0d7aa2d711d4d0411851ed3ac23d44f2f752ffd
grvk28 <gaurav91226591@gmail.com>
2022-12-22 14:22:59 +0530

    edited2

--- src/app/src/App.js
+++ src/app/src/App.js
@@ -1,29 +1,26 @@
-import './App.css';
 import logo from './logo.svg';
+import './App.css';
+import Form from './form';
+import List from './list';
 
-
-export function App() {
-  return (
-    <div className="App">
-      <div>
-        <h1>List of TODOs</h1>
-        <li>Learn Docker</li>
-        <li>Learn React</li>
-      </div>
-      <div>
-        <h1>Create a ToDo</h1>
-        <form>
-          <div>
-            <label for="todo">ToDo: </label>
-            <input type="text" />
-          </div>
-          <div style={{"marginTop": "5px"}}>
-            <button>Add ToDo!</button>
-          </div>
-        </form>
-      </div>
-    </div>
-  );
-}
+function App(){
+	
+		return (
+			<div className="body">
+       <Form/> 
+       <hr
+                    style={{
+                        color: "#000000",
+                        backgroundColor: "#000000",
+                        height: 0.5,
+                        borderColor: "#000000",
+                        width:'100%'
+                    }}
+                />
+                <h2>List</h2>
+       <List/> 
+			</div>
+		);
+	}
 
 export default App;
